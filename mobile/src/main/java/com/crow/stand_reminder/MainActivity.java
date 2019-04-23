576341ac4a654228ffa4c89d9846538d32fd82f0
kraxarn <me@kraxarn.com>
2019-04-23 22:52:52 +0200

    Default project files

---
+++ mobile/src/main/java/com/crow/stand_reminder/MainActivity.java
@@ -0,0 +1,16 @@
+package com.crow.stand_reminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
