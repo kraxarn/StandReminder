c76a37e435dd888b699bdbb05da42d68f4aa00c8
kraxarn <me@kraxarn.com>
2019-04-24 23:30:48 +0200

    Add ability to select wear device to use

--- mobile/src/main/java/com/crow/stand_reminder/fragment/SettingsFragment.java
+++ mobile/src/main/java/com/crow/stand_reminder/fragment/SettingsFragment.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.crow.stand_reminder.BuildConfig;
 import com.crow.stand_reminder.R;
+import com.crow.stand_reminder.tool.WearTools;
+import com.google.android.gms.wearable.Node;
 
-import java.util.Locale;
+import java.util.List;
 
 public class SettingsFragment extends PreferenceFragmentCompat
 {
@@ -52,6 +56,38 @@ public class SettingsFragment extends PreferenceFragmentCompat
 
 			return true;
 		});
+
+		// Fill list with wear devices
+		ListPreference wearDevice = findPreference("wear_device");
+		if (wearDevice != null)
+		{
+			new Thread(() ->
+			{
+				List<Node> nodes = WearTools.getNodes(getContext());
+				if (nodes == null || nodes.size() == 0)
+				{
+					if (getActivity() != null)
+						getActivity().runOnUiThread(() -> wearDevice.setEnabled(false));
+					return;
+				}
+
+				// Node entries with strings to show in dialog
+				String[] entries = new String[nodes.size()];
+				int entryIndex = 0;
+				// Node IDs to save to preferences
+				String[] values = new String[nodes.size()];
+				int valueIndex = 0;
+
+				for (Node node : nodes)
+				{
+					entries[entryIndex++] = node.getDisplayName();
+					values[valueIndex++]  = node.getId();
+				}
+
+				wearDevice.setEntries(entries);
+				wearDevice.setEntryValues(values);
+			}).start();
+		}
 
 		// Set correct version info
 		Preference version = findPreference("about_version");
