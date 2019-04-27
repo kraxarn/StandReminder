7f6f396e9d3d2c591fd9a4b8d947eeed4ef2263d
kraxarn <me@kraxarn.com>
2019-04-27 15:33:17 +0200

    Rewrite Ongoing notification manager in Kotlin

---
+++ mobile/src/main/java/com/crow/stand_reminder/service/OngoingNotificationManager.kt
@@ -0,0 +1,55 @@
+package com.crow.stand_reminder.service
+
+import android.app.Notification
+import android.content.Context
+
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+import com.crow.stand_reminder.R
+import com.crow.stand_reminder.tool.NotificationTools
+
+object OngoingNotificationManager
+{
+    private var enabled = false
+
+    const val notificationId = 1010
+
+    const val channelId = "persistent"
+
+    private const val name = "Persistent"
+
+    fun create(context: Context)
+    {
+        // Just create a notification channel, don't show anything yet
+        // TODO: NO
+        NotificationTools.createNotificationChannel(context, channelId, name, Notification.PRIORITY_DEFAULT)
+        // Enabled
+        enabled = true
+    }
+
+    fun update(context: Context, text: String)
+    {
+        if (enabled)
+            NotificationTools.show(
+                    context,
+                    notificationId,
+                    channelId,
+                    R.drawable.ic_launcher_foreground,
+                    context.getString(R.string.app_name),
+                    text,
+                    NotificationCompat.PRIORITY_DEFAULT,
+                    true)
+    }
+
+    fun resume()
+    {
+        enabled = true
+    }
+
+    fun pause(context: Context)
+    {
+        NotificationManagerCompat.from(context).cancel(notificationId)
+        enabled = false
+    }
+}
