1195ba0efe7ad610a82f8e9c2ff58cabfd21d3ea
kraxarn <me@kraxarn.com>
2019-05-02 00:23:52 +0200

    Remove unused type converters

--- mobile/src/main/java/com/crow/stand_reminder/data/DataConverter.kt
+++ mobile/src/main/java/com/crow/stand_reminder/data/DataConverter.kt
@@ -6,12 +6,6 @@ import java.util.*
 class DataConverter
 {
 	@TypeConverter
-	fun toDate(value: Long): Date = value.let(::Date)
-
-	@TypeConverter
-	fun fromDate(value: Date): Long = value.time
-
-	@TypeConverter
 	fun toCalendar(value: Long): Calendar =
 			Calendar.getInstance().apply { timeInMillis = value }
 
