d45cefc78afc06b4115273e201ea8507097a8e74
kraxarn <me@kraxarn.com>
2019-07-10 19:06:52 +0200

    Add custom toString

--- mobile/src/main/java/com/crow/stand_reminder/data/CompletedHour.kt
+++ mobile/src/main/java/com/crow/stand_reminder/data/CompletedHour.kt
@@ -2,7 +2,12 @@ package com.crow.stand_reminder.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.crow.stand_reminder.tool.CalendarTools
 import java.util.*
 
 @Entity
 data class CompletedHour(@PrimaryKey val date: Calendar, val source: ValueSource)
+{
+	override fun toString(): String =
+		"${CalendarTools.format(date, CalendarTools.Format.FULL)}: $source"
+}
